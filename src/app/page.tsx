776fe5cdb1a19587d8ae54d38caa81af1b3e34f2
Michael Shaevitch <michael.shaevitch@gmail.com>
2024-11-18 20:09:59 -0500

    Add main landmark
    Add <main> landmark component for accessibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 export default function Home() {
     return (
         <div>
-            <div className="min-h-screen w-full flex items-center justify-center">
-                <div className="text-4xl font-bold">
-                    Coming soon...
+            <main>
+                <div className="min-h-screen w-full flex items-center justify-center">
+                    <div className="text-4xl font-bold">
+                        Coming soon...
+                    </div>
                 </div>
-            </div>
+            </main>
             <div className="absolute flex gap-3 left-1/2 -translate-x-1/2 bottom-10 font-[family-name:var(--font-geist-sans)]">
                 <a
                     href="https://github.com/mshaevitch"
